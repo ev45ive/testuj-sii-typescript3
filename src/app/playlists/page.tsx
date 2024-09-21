0fb6244cc9900bad7433e464b6c6821a711dcc36
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-21 15:46:16 +0200

    Tailwind

---
+++ src/app/playlists/page.tsx
@@ -0,0 +1,7 @@
+export default function PlaylistsView() {
+  return (
+    <div className="container">
+      <h1 className="text-2xl text-red-400 leading-loose mb-4">Playlists</h1>
+    </div>
+  );
+}
