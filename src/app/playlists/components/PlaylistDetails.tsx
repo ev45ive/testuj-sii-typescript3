a02ccd543d1172d5c8fb8b813907afbff87c6f62
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-21 16:07:52 +0200

    Exercise: React JSX

--- src/app/playlists/components/PlaylistDetails.tsx
+++ src/app/playlists/components/PlaylistDetails.tsx
@@ -1,16 +1,36 @@
-
-
 // tsrafce
 
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const PlaylistDetails = (props: Props) => {
+  const playlist = {
+    id: "123",
+    name: "Playlist 123",
+    public: true,
+    description: "Awesome playlist",
+  };
+
   return (
-    <div>PlaylistDetails</div>
-  )
-}
+    <div>
+      {/* .grid.gap-5>.grid.gap-2*3>strong{Name}+div{Playlist $$$} */}
+      <div className="grid gap-5">
+        <div className="grid gap-2">
+          <strong>Name</strong>
+          <div>{playlist.name}</div>
+        </div>
+        <div className="grid gap-2">
+          <strong>Public</strong>
+          <div>Yes</div>
+        </div>
+        <div className="grid gap-2">
+          <strong>Description</strong>
+          <div>BEest Playlist </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PlaylistDetails
-
+export default PlaylistDetails;
