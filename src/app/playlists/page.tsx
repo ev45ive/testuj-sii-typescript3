5dc5db28d090ac05d3411de8714845b6fe2b40c1
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-21 15:54:54 +0200

    Extensions  @recommended

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,7 +1,11 @@
 export default function PlaylistsView() {
   return (
     <div className="container">
-      <h1 className="text-2xl text-red-400 leading-loose mb-4">Playlists</h1>
+      <h1 className="text-2xl text-red-400 leading-loose mb-4">
+        Playlists
+      </h1>
+
+      
     </div>
   );
 }
