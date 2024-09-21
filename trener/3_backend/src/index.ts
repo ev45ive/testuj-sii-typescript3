3df7ec1a3a32a817a32b9ead477a0ad850fcf6bb
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-21 13:54:15 +0200

    tsc --watch , node --watch  // npm i nodemon

--- trener/3_backend/src/index.ts
+++ trener/3_backend/src/index.ts
@@ -1,8 +1,25 @@
+import express from "express";
 
+const app = express();
 
-// window // ReferenceError: window is not defined
-globalThis
-
-const user = process.argv[2] || 'Stranger'
+const user = process.argv[2] || "Stranger";
 
 console.log(`Hello ${user}`);
+
+app.get("/", (req, res) => {
+  res.write(/* html */ `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+    <h1> ❤️❤️❤️ Welcome ${user} ❤️❤️❤️</h1>`);
+  res.end();
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on http://localhost:8080/ <3 <3 <3");
+});
