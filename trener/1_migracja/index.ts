7131e9cf70cda107a240f0dab225ad7895891d0b
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-21 10:33:05 +0200

    Exercise: Compiling TS to JS

---
+++ trener/1_migracja/index.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+
+add(1,1)
+
+// Argument of type 'string' is not assignable to parameter of type 'number'. ts(2345)
+// add('ala','makota')
