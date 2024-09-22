2b52b7b4a929b5f6e89ccb6f3497860672b4f09d
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 14:33:12 +0200

    Monoid

--- src/app/model/Playlist.tsx
+++ src/app/model/Playlist.tsx
@@ -4,15 +4,3 @@ export interface Playlist {
   public: boolean;
   description: string;
 }
-
-
-// Semigroup // Monoid 
-'' + 'text' + 'text' + '';
-0 + 123 + 123 + 123 + 0 ;
-1 * 100 * 23* 23 * 23 * 1 ;
-([] as number[]).concat([123]).concat([2323]).concat([]);
-
-// Null Object Pattern
-
-// [setState] = useState(EMPTY_PLAYLIST)
-// setState(playlist)
---
+++ trener/examples/monoid.md
@@ -0,0 +1,14 @@
+```js
+
+
+// Semigroup // Monoid 
+'' + 'text' + 'text' + '';
+0 + 123 + 123 + 123 + 0 ;
+1 * 100 * 23* 23 * 23 * 1 ;
+([] as number[]).concat([123]).concat([2323]).concat([]);
+
+// Null Object Pattern
+
+// [setState] = useState(EMPTY_PLAYLIST)
+// setState(playlist)
+```
