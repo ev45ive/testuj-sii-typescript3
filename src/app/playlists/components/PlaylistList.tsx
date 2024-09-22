93ca56fabcf8c18d2dfd4b675b38b6dcdd2b248b
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 11:26:18 +0200

    Exercise: Fix Props type - add missing prop types

--- src/app/playlists/components/PlaylistList.tsx
+++ src/app/playlists/components/PlaylistList.tsx
@@ -4,17 +4,28 @@ import React from "react";
 
 type Props = {
   playlists: Playlist[];
+  // ????
+  // ???
 };
 
-const PlaylistList = ({ playlists }: Props) => {
+const PlaylistList = ({ playlists, onSelect, selectedId }: Props) => {
   return (
     <div>
       <div className="divide-y divide-gray-300 divide-solid ">
         {playlists.map((playlist, index) => (
-          <p className="px-2 py-5" key={playlist.id}>{index+1}. {playlist.name}</p>
+          <p
+            className={`px-2 py-5 ${
+              selectedId == playlist.id ? "bg-slate-600 text-black" : ""
+            }`}
+            onClick={() => {
+              onSelect(playlist.id);
+            }}
+            key={playlist.id}
+          >
+            {index + 1}. {playlist.name}
+          </p>
         ))}
       </div>
-
     </div>
   );
 };
--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -8,13 +8,22 @@ export default function PlaylistsView() {
   const playlists: Playlist[] = mockPlaylists;
   const selected = playlists[0];
 
+  const selectPlaylistById = (id: string) => {
+    const selected = playlists.find((p) => p.id == id)!;
+    console.log("selected", selected);
+  };
+
   return (
     <div className="container">
       <h1 className="text-2xl text-red-400 leading-loose mb-4">Playlists</h1>
 
       <div className="grid grid-cols-2 gap-5">
         <div>
-          <PlaylistList playlists={playlists} />
+          <PlaylistList
+            playlists={playlists}
+            selectedId={"234"}
+            onSelect={selectPlaylistById}
+          />
         </div>
         <div>
           <PlaylistDetails playlist={selected} />
