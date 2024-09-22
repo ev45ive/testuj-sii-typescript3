f8b491e4b67ade87ff01234bbcc6fb209fb7a853
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 10:51:11 +0200

    // Type Narrowing // Point & Record<"length", unknown>

--- src/app/model/Playlist.tsx
+++ src/app/model/Playlist.tsx
@@ -17,11 +17,28 @@ let p: Point = { x: 123, y: 234 };
 let v: Vector = { x: 123, y: 234, length: 123 };
 
 // Covariance
-p = v;
 // v = p; // Property 'length' is missing in type 'Point' but required in type 'Vector'.ts(2741)
+p = v;
 
+(p as Vector).length; // OK?
+
+let x = p as Vector | Point;
+
+// x.length // Error
+
+// Type Narrowing
+if ("length" in p) p; // Point & Record<"length", unknown>
+if ("length" in p && typeof p.length === "string") p.length; // Point & Record<"length", string>
+if ("length" in x) x.length;
 
 // Duck typing
-interface Plane{ fly: true, takePasangers:true }
-interface Bird{ fly: true }
-interface Duck{ fly: true }
+interface Plane {
+  fly: true;
+  takePasangers: true;
+}
+interface Bird {
+  fly: true;
+}
+interface Duck {
+  fly: true;
+}
