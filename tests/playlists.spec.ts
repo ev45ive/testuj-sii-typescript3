8e1b88af3d79112551595a7fa0c581b2c2ae76db
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 16:24:46 +0200

    Scenario Definition // Steps Definition:

--- tests/playlists.spec.ts
+++ tests/playlists.spec.ts
@@ -1,5 +1,6 @@
+import { Playlist } from "@/app/model/Playlist";
 import { mockPlaylists } from "@/app/playlists/mockPlaylists";
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
 test("Follow Playlists link", async ({ page }) => {
   await page.goto("http://localhost:3000/");
@@ -24,14 +25,26 @@ test("Playlists page has title", async ({ page }) => {
 test("should show selected playlist", async ({ page }) => {
   await page.goto("http://localhost:3000/playlists");
 
-  const playlistItems = page.locator("[data-testid=playlist-item]");
+  // Scenario Definition:
+  await selectPlaylistOnList(page, mockPlaylists[0].name);
+  await assertPlaylistDetails(page, mockPlaylists[0]);
 
-  const mock = mockPlaylists[0];
-  const selection = playlistItems.getByText(mock.name);
+  await selectPlaylistOnList(page, mockPlaylists[1].name);
+  await assertPlaylistDetails(page, mockPlaylists[1]);
 
-  await selection.click();
+  await selectPlaylistOnList(page, mockPlaylists[2].name);
+  await assertPlaylistDetails(page, mockPlaylists[2]);
+});
 
+// Steps Definition:
+async function assertPlaylistDetails(page: Page, mock: Playlist) {
   const playlistName = page.getByTestId("playlist-details-name");
 
   await expect(playlistName).toHaveText(mock.name);
-});
+}
+
+async function selectPlaylistOnList(page: Page, name: string) {
+  const playlistItems = page.locator("[data-testid=playlist-item]");
+  const selection = playlistItems.getByText(name);
+  await selection.click();
+}
