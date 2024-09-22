a8a01bfdbbdf226428a6e2efd059820f1a09e9f4
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 09:28:47 +0200

    map + key

--- src/app/playlists/components/PlaylistList.tsx
+++ src/app/playlists/components/PlaylistList.tsx
@@ -11,7 +11,7 @@ const PlaylistList = ({ playlists }: Props) => {
     <div>
       <div className="divide-y divide-gray-300 divide-solid ">
         {playlists.map((playlist, index) => (
-          <p className="px-2 py-5">{index+1}. {playlist.name}</p>
+          <p className="px-2 py-5" key={playlist.id}>{index+1}. {playlist.name}</p>
         ))}
       </div>
 
