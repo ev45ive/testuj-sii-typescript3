b0177367baa659bae21eb7afe2d011efeafb25ad
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 15:59:16 +0200

    Working tests ( asnyc/await )

--- tests/playlists.spec.ts
+++ tests/playlists.spec.ts
@@ -1,7 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test("Playlists page has title", async ({ page }) => {
-  page.goto("http://localhost:3000/");
+test("Follow Playlists link", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
 
-  expect(page.getByText("Playlists")).toBeVisible();
+  //   expect(true).toBeTruthy() // Sync - no promise, no await needed
+
+  await page.getByRole("link", { name: /Playlists/ }).click();
+
+  await page.waitForURL(/\/playlists/);
+
+  await expect(page.getByText("Playlists")).toBeVisible();
+});
+
+test("Playlists page has title", async ({ page }) => {
+  await page.goto("http://localhost:3000/playlists");
+
+  await expect(page.getByText("Playlists")).toBeVisible();
+  
+  await expect(page.getByText("No playlist selected")).toBeVisible();
+
 });
