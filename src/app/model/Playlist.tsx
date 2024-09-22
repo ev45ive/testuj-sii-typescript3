37c61b84cbbd5d0aa52d73d6203b466ccf5f8b12
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-22 12:10:27 +0200

    const inception: TypedBox<TypedBox<string>>

--- src/app/model/Playlist.tsx
+++ src/app/model/Playlist.tsx
@@ -5,3 +5,28 @@ export interface Playlist {
   description: string;
 }
 
+const arr1: number[] = [1, 2, 3];
+// arr.push('23') // error
+const arr2: string[] = ["a", "b" /*  123 - error */];
+const arr3: Array<string> = ["a", "b" /*  123 - error */];
+
+type Box = { value: any };
+// type stringBox = { value: string };
+// type numberBox = { value: number };
+// type stringArrayBox = { value: Array<string> };
+
+const typeForgettingBox: Box = { value: "pączki" };
+
+const suprise = typeForgettingBox.value; // any
+
+type TypedBox<T> = { value: T };
+
+const memoryBox: TypedBox<string> = { value: "123" };
+memoryBox.value.search("123");
+
+const inception: TypedBox<TypedBox<string>> = { value: { value: "DIcaprio" } };
+
+let randomBox = {} as TypedBox<string> | TypedBox<number>;
+if (typeof randomBox.value === "string") {
+  randomBox.value; // string
+}
