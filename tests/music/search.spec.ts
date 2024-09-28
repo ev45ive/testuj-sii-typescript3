295e8324f4ec6831282e79e01a2c07cc16d28d8f
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 14:10:08 +0200

    Testing search

--- tests/music/search.spec.ts
+++ tests/music/search.spec.ts
@@ -1,7 +1,25 @@
-import test from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("/music/search");
+});
 
-test('Empty search box', async ({page}) => {
-    // TODO:
-})
+test("Empty search box", async ({ page }) => {
+  // TODO:
+  await expect(page.getByPlaceholder("Search")).toBeEmpty();
+});
 
+test("Not Logged In", async ({ page }) => {
+  await page.getByPlaceholder("Search").fill("test");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  const NoTokenProvided = page.getByText("No token provided");
+  await expect(NoTokenProvided).toBeVisible();
+});
+
+test("Searching", async ({ page }) => {
+  await page.getByPlaceholder("Search").click();
+  await page.getByPlaceholder("Search").fill("test");
+  await page.getByRole("button", { name: "Search" }).click(); 
+  
+});
