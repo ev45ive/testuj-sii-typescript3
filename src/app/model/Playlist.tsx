4f9347ca3f2f63bd18c11e2252ca27217ae70cce
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 09:19:34 +0200

    JSDOC - Typescript is documentation  "at fingertips"

--- src/app/model/Playlist.tsx
+++ src/app/model/Playlist.tsx
@@ -1,6 +1,10 @@
 export interface Playlist {
   id: string;
   name: string;
+  /**
+   * Is playlist only mine or visible to others
+   * @see https://developer.spotify.com/documentation/web-api/concepts/playlists
+   */
   public: boolean;
   description: string;
 }
