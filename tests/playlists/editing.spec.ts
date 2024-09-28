874dbdaab237b6fe81fadb3753b2f60962010775
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 09:50:04 +0200

    Locators, Assertions, Promises

--- tests/playlists/editing.spec.ts
+++ tests/playlists/editing.spec.ts
@@ -1,15 +1,25 @@
-
 import { test, expect, Page } from "@playwright/test";
 
-test('Opening editor', async ({page}) => {
-  
-})
+test("Opening editor", async ({ page }) => {
+  await page.goto("http://localhost:3000/playlists");
 
-test('Editor - Canceling', async ({page}) => {
-  
-})
+  await expect(
+    page.getByTestId("playlist-item").filter({
+      hasText: "Playlist 123",
+    })
+  ).toBeVisible();
 
-test('Editor - Saving', async ({page}) => {
-  
-})
+  await page.getByText("Playlist 234").click();
 
+  await expect(page.getByTestId("playlist-details-name")).toContainText(
+    "Playlist 234"
+  );
+  await page.getByRole("button", { name: "Edit" }).click();
+  await expect(page.locator("textarea")).toContainText("Best playlist");
+  await expect(page.locator('input[type="text"]')).toHaveValue("Playlist 234");
+  await page.getByRole("button", { name: "Cancel" }).click();
+});
+
+test("Editor - Canceling", async ({ page }) => {});
+
+test("Editor - Saving", async ({ page }) => {});
