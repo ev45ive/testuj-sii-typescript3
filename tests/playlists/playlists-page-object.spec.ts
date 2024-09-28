5d22ba61c3d19b150dfb3d7932aa1afbd4b22496
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 11:15:19 +0200

    Exercise: POM assertions - select item

--- tests/playlists/playlists-page-object.spec.ts
+++ tests/playlists/playlists-page-object.spec.ts
@@ -1,16 +1,32 @@
-import test, { Page } from "@playwright/test";
+import { Playlist } from "@/app/model/Playlist";
+import { mockPlaylists } from "@/app/playlists/mockPlaylists";
+import test, { expect, Page } from "@playwright/test";
 
 test.describe("Playlists Page Object", () => {
   test("Testing playlists", async ({ page }) => {
-    const pom = new PlaylistsPageObject(page);
-    pom.loadPage();
+    const pom = new PlaylistsPageObject(page, mockPlaylists);
+    await pom.loadPage();
+    await pom.countListItems(3);
+
+    await pom.selectItemByName("Playlist 123");
   });
 });
 
 class PlaylistsPageObject {
-  constructor(private page: Page) {}
+  constructor(private page: Page, private mocks: Playlist[]) {}
 
-  loadPage() {
-    this.page.goto("/playlists");
+  listItems = this.page.getByTestId("playlist-item");
+
+  async selectItemByName(arg0: string) {
+  
+     // TODO: from playists.spec.ts
+}
+
+  async countListItems(count: number) {
+    return expect(this.listItems).toHaveCount(count);
+  }
+
+  async loadPage() {
+    await this.page.goto("/playlists");
   }
 }
