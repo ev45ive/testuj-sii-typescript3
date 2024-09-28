d857c093e84a13e338ff3e0768bf48f645781e56
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 15:33:01 +0200

    toBe, toStrictEqual, toEqual, arrayContaining , objectContaining, expect.any(String),

---
+++ tests/music/api.spec.ts
@@ -0,0 +1,32 @@
+import test, { expect } from "@playwright/test";
+
+test("Test login api token", async ({ page, request }) => {
+  const res = await request.get("/api/token", {});
+
+  const data: APITokenResponse = await res.json();
+
+  // expect(data).toBe('placki')
+  // expect(data).toBe({  // Object.is equality !!
+  // expect(data).toStrictEqual
+  // expect(data).toEqual
+  //   expect(data).toEqual({
+  //     access_token: "BQBie...4L7EpI",
+  //     expires_in: 3600,
+  //     token_type: "Bearer",
+  //   });
+  expect([data]).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        access_token: expect.any(String),
+        expires_in: expect.any(Number),
+        token_type: "Bearer",
+      }),
+    ])
+  );
+});
+
+export interface APITokenResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+}
