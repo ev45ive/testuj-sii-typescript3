24f8e1b1da69f2eb80e7effc6f466330d36937ad
Mateusz Kulesza <ev45ive@gmail.com>
2024-09-28 09:31:52 +0200

    Exercise: testing editor

---
+++ tests/playlists/editing.spec.ts
@@ -0,0 +1,15 @@
+
+import { test, expect, Page } from "@playwright/test";
+
+test('Opening editor', async ({page}) => {
+  
+})
+
+test('Editor - Canceling', async ({page}) => {
+  
+})
+
+test('Editor - Saving', async ({page}) => {
+  
+})
+
